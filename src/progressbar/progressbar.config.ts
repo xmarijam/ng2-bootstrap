0338b2e2770b2bd064d08b9807c526c3c4b93f17
Marija <marija@eutelnet.biz>
2016-12-23 14:33:00 +0100

    packaged

--- src/progressbar/progressbar.config.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class ProgressbarConfig {
-  /** if `true` changing value of progress bar will be animated (note: not supported by Bootstrap 4) */
-  public animate: Boolean = true;
-  /** maximum total value of progress element */
-  public max: number = 100;
-}
